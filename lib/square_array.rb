3deff4227337731ed38f8dc8d3a11acb433965d8
Jonah Offitzer <joffitzer@gmail.com>
2019-10-16 01:29:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   counter = 0
-  while array[counter] do
-    p array[counter] ** 2
+  while numbers[counter] do
+    p numbers[counter] ** 2
     counter += 1 
   end
 end
