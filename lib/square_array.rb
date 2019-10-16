9af1567374f9771330e9e3aadaf0f50d7d601517
Jonah Offitzer <joffitzer@gmail.com>
2019-10-16 02:56:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(numbers)
   new_array = []
   while numbers[counter] do
     numbers[counter] ** 2
-    counter += 1 
     new_array << numbers[counter] ** 2
+    counter += 1 
   end
   new_array
 end
